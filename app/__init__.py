abebf4ea17a27a05a33b15bb96b1346c06352026
Arami Girett <aramigirett@gmail.com>
2024-09-24 21:11:16 -0400

    add: referencial ocupaciones

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from app.rutas.referenciales.correo.correo_routes import cormod #correo
 from app.rutas.referenciales.telefono.telefono_routes import telmod  #telefono
 from app.rutas.referenciales.direccion.direccion_routes import diremod #direccion
 from app.rutas.referenciales.nacionalidad.nacionalidad_routes import naciomod  #nacionalidad
+from app.rutas.referenciales.ocupacion.ocupacion_routes import ocupmod  #ocupacion
 
 
 # registrar referenciales
@@ -19,6 +20,7 @@ app.register_blueprint(cormod, url_prefix=f'{modulo0}/correo') #correo
 app.register_blueprint(telmod, url_prefix=f'{modulo0}/direccion')  #telefono
 app.register_blueprint(diremod, url_prefix=f'{modulo0}/direccion') #direccion
 app.register_blueprint(naciomod, url_prefix=f'{modulo0}/nacionalidad')  #nacionalidad
+app.register_blueprint(ocupmod, url_prefix=f'{modulo0}/ocupacion')  #ocupacion
 
 
 #ciudad
@@ -38,6 +40,9 @@ from.rutas.referenciales.direccion.direccion_api import direapi
 
 #nacionalidad
 from app.rutas.referenciales.nacionalidad.nacionalidad_api import nacioapi
+
+#ocupacion
+from app.rutas.referenciales.ocupacion.ocupacion_api import ocupapi
 
 # APIS v1
 #Ciudad
@@ -64,3 +69,7 @@ app.register_blueprint(direapi, url_prefix=version1)
 #nacionalidad
 version1 = '/api/v1'
 app.register_blueprint(nacioapi, url_prefix=version1)
+
+#ocupacion
+version1 = '/api/v1'
+app.register_blueprint(ocupapi, url_prefix=version1)
