24e33a5222f9306e95236e2957c62f5f9a1c3fa6
Arami Girett <aramigirett@gmail.com>
2024-09-29 21:40:42 -0400

    cambio en referenciales

--- app/__init__.py
+++ app/__init__.py
@@ -5,23 +5,27 @@ app = Flask(__name__)
 # importar referenciales
 from app.rutas.referenciales.ciudad.ciudad_routes import ciumod #ciudad
 from app.rutas.referenciales.paises.pais_routes import paimod   #pais
-from app.rutas.referenciales.correo.correo_routes import cormod #correo
-from app.rutas.referenciales.telefono.telefono_routes import telmod  #telefono
-from app.rutas.referenciales.direccion.direccion_routes import diremod #direccion
 from app.rutas.referenciales.nacionalidad.nacionalidad_routes import naciomod  #nacionalidad
 from app.rutas.referenciales.ocupacion.ocupacion_routes import ocupmod  #ocupacion
-
+from app.rutas.referenciales.estado_civil.estado_civil_routes import estacivmod  #estado civil
+from app.rutas.referenciales.sexo.sexo_routes import sexmod  #sexo
+from app.rutas.referenciales.estado_cita.estado_cita_routes import estacitmod  #estado de la cita
+from app.rutas.referenciales.persona.persona_routes import persmod  #persona
+from app.rutas.referenciales.especialidad.especialidad_routes import especimod  #especialidad
+from app.rutas.referenciales.dia.dia_routes import diamod  #dia
 
 # registrar referenciales
 modulo0 = '/referenciales'
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad') #ciudad
 app.register_blueprint(paimod, url_prefix=f'{modulo0}/paises') #pais
-app.register_blueprint(cormod, url_prefix=f'{modulo0}/correo') #correo
-app.register_blueprint(telmod, url_prefix=f'{modulo0}/direccion')  #telefono
-app.register_blueprint(diremod, url_prefix=f'{modulo0}/direccion') #direccion
 app.register_blueprint(naciomod, url_prefix=f'{modulo0}/nacionalidad')  #nacionalidad
 app.register_blueprint(ocupmod, url_prefix=f'{modulo0}/ocupacion')  #ocupacion
-
+app.register_blueprint(estacivmod, url_prefix=f'{modulo0}/estadocivil')  #estado civil
+app.register_blueprint(sexmod, url_prefix=f'{modulo0}/sexo')  #sexo
+app.register_blueprint(estacitmod, url_prefix=f'{modulo0}/estadocita')  #estado de la cita
+app.register_blueprint(persmod, url_prefix=f'{modulo0}/persona') #persona
+app.register_blueprint(especimod, url_prefix=f'{modulo0}/especialidad') #especialidad
+app.register_blueprint(diamod, url_prefix=f'{modulo0}/dia') #dia
 
 #ciudad
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
@@ -29,20 +33,29 @@ from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
 #pais
 from app.rutas.referenciales.paises.pais_api import paisapi
 
-#coreo
-from app.rutas.referenciales.correo.correo_api import correoapi
-
-#telefono
-from app.rutas.referenciales.telefono.telefono_api import telapi
-
-#direccion
-from.rutas.referenciales.direccion.direccion_api import direapi
-
 #nacionalidad
 from app.rutas.referenciales.nacionalidad.nacionalidad_api import nacioapi
 
-#ocupacion
+#nacionalidad
 from app.rutas.referenciales.ocupacion.ocupacion_api import ocupapi
+
+#estado civil
+from app.rutas.referenciales.estado_civil.estado_civil_api import estacivapi
+
+#sexo
+from app.rutas.referenciales.sexo.sexo_api import sexapi
+
+#estado de la cita
+from app.rutas.referenciales.estado_cita.estado_cita_api import estacitapi
+
+#persona
+from app.rutas.referenciales.persona.persona_api import persapi
+
+#especialidad
+from app.rutas.referenciales.especialidad.especialidad_api import especiapi
+
+#dia
+from app.rutas.referenciales.dia.dia_api import diaapi
 
 # APIS v1
 #Ciudad
@@ -53,19 +66,6 @@ app.register_blueprint(ciuapi, url_prefix=version1)
 version1 = '/api/v1'
 app.register_blueprint(paisapi, url_prefix=version1)
 
-#correo
-version1 = '/api/v1'
-app.register_blueprint(correoapi, url_prefix=version1)
-
-
-#telefono
-version1 = '/api/v1'
-app.register_blueprint(telapi, url_prefix=version1)
-
-#direccion
-version1 = '/api/v1'
-app.register_blueprint(direapi, url_prefix=version1)
-
 #nacionalidad
 version1 = '/api/v1'
 app.register_blueprint(nacioapi, url_prefix=version1)
@@ -73,3 +73,27 @@ app.register_blueprint(nacioapi, url_prefix=version1)
 #ocupacion
 version1 = '/api/v1'
 app.register_blueprint(ocupapi, url_prefix=version1)
+
+#Estado civil
+version1 = '/api/v1'
+app.register_blueprint(estacivapi, url_prefix=version1)
+
+#sexo
+version1 = '/api/v1'
+app.register_blueprint(sexapi, url_prefix=version1)
+
+#Estado de la cita
+version1 = '/api/v1'
+app.register_blueprint(estacitapi, url_prefix=version1)
+
+#persona
+version1 = '/api/v1'
+app.register_blueprint(persapi, url_prefix=version1)
+
+#especialidad
+version1 = '/api/v1'
+app.register_blueprint(especiapi, url_prefix=version1)
+
+#dia
+version1 = '/api/v1'
+app.register_blueprint(diaapi, url_prefix=version1)
